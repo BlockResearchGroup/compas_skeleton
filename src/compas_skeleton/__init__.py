9bccf210fd3a88a7b5958ab7848aef1e1ce815ef
tomvanmele <tom.v.mele@gmail.com>
2025-02-03 21:24:54 +0100

    Bump version to 2.0.1

--- src/compas_skeleton/__init__.py
+++ src/compas_skeleton/__init__.py
@@ -5,7 +5,7 @@ __author__ = ["Wenqian Yang"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "yang@arch.ethz.ch"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 
 HERE = os.path.dirname(__file__)
